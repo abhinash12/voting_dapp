25c00566983ec89140b8ed34d56b0925cf0d2199
abhinash <abhi@macs-MacBook.local>
2018-05-29 02:27:26 -0400

    Voting

---
+++ README.md
@@ -0,0 +1,16 @@
+Creating your first basic decentralized voting application
+
+Used:
+•	Solidity
+•	Ganache
+•	Node.js
+•	Html
+
+Steps:
+
+•	Write smart contract using solidity.
+•	Compile the contract which generates bytecode, abi among other things.
+•	Ganache gives us a blockchain network on which we could deploy the smart contract.
+•	We deploy the bytecode on the blockchain.
+•	After deploying we could interact from the console and run the functions we declared in the smart contracts.
+•	We develop a basic web page for web interaction and we use web3 rpc calls to interact with the blockchain.
--- contracts/index.js
+++ contracts/index.js
@@ -1,8 +1,8 @@
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 abi = JSON.parse('[{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]')
 VotingContract = web3.eth.contract(abi);
-contractInstance = VotingContract.at('0x48a5194658639b770bcd186ad7a2a9a0be0c48fa');
-candidates = {"Rama": "candidate-1", "Nick": "candidate-2", "Jose": "candidate-3", "Dan": "candidate-4", "Alan": "candidate-5"}
+contractInstance = VotingContract.at('0x51d02dcf0adcb7385b30a03571b2178ae83661fc');
+candidates = {"Abhi": "candidate-1", "Alan": "candidate-2", "Suz": "candidate-3", "Dan": "candidate-4", "Ani": "candidate-5"}
 
 function voteForCandidate(candidate) {
  candidateName = $("#candidate").val();
